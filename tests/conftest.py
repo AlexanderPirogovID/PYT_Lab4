6c48dabc2a22167beb60c00492a4efb2f2f9e401
AlexanderPirogovID <andrey.v.pirogov@gmail.com>
2026-05-11 10:44:13 +0300

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,21 @@
+import sys
+from pathlib import Path
+
+# Добавляем корень проекта в PYTHONPATH
+sys.path.insert(0, str(Path(__file__).parent.parent))
+
+from app.services.processor import DataProcessor
+
+
+
+
+
+
+
+
+
+
+
 import pytest
 from app.services.processor import DataProcessor
 
